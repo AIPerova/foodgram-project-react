83d4f80f6578aa78c73a66f24383fcd6ced61714
Алёна Перова <perovaai31@yandex.ru>
2023-09-12 18:57:54 +0300

    +  полный сериализатор для рецептов

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,14 @@
 from django.db.models import F
+from django.shortcuts import get_object_or_404
 
-from rest_framework.fields import SerializerMethodField
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import IntegerField, SerializerMethodField
+from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
 from drf_extra_fields.fields import Base64ImageField
 
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, IngredientToRecipe, Recipe, Tag
 
 
 class UserSerializer(ModelSerializer):
@@ -76,5 +79,85 @@ class RecipeReadSerializer(ModelSerializer):
         return user.shopping_cart.filter(recipe=obj).exists()
 
 
+class IngredientToRecipeWriteSerializer(ModelSerializer):
+    id = IntegerField(write_only=True)
+
+    class Meta:
+        model = IngredientToRecipe
+        fields = ('id', 'amount')
+
+
 class RecipeWriteSerializer(ModelSerializer):
-    pass
+    '''Для редактирование, добавления рецепта.'''
+    tags = PrimaryKeyRelatedField(queryset=Tag.objects.all(),
+                                  many=True)
+    author = UserSerializer(read_only=True)
+    ingredients = IngredientToRecipeWriteSerializer(many=True)
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+    def validate_ingredients(self, value):
+        ingredients = value
+        if not ingredients:
+            raise ValidationError({
+                'ingredients': 'Нельзя приготовить из ничего :(!'
+            })
+        ingredients_list = []
+        for item in ingredients:
+            ingredient = get_object_or_404(Ingredient, id=item['id'])
+            if ingredient in ingredients_list:
+                raise ValidationError({
+                    'ingredients': 'Это мы уже записали!'
+                })
+            if int(item['amount']) <= 0:
+                raise ValidationError({
+                    'amount': 'Надо положить больше чем ничего!'
+                })
+            ingredients_list.append(ingredient)
+        return value
+
+    def validate_tags(self, value):
+        tags = value
+        if not tags:
+            raise ValidationError({'tags': 'Выбери хоть один тег!'})
+        tags_list = []
+        for tag in tags:
+            if tag in tags_list:
+                raise ValidationError(
+                    {'tags': 'Теги должны быть уникальными!'}
+                    )
+            tags_list.append(tag)
+        return value
+
+    def add_ingredients_and_tags(self, tags, ingredients, recipe):
+        for tag in tags:
+            recipe.tags.add(tag)
+            recipe.save()
+        for ingredient in ingredients:
+            IngredientToRecipe.objects.create(
+                ingredient_id=ingredient.get('id'),
+                amount=ingredient.get('amount'),
+                recipe=recipe
+            )
+        return recipe
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags')
+        recipe = Recipe.objects.create(**validated_data)
+        return self.add_ingredients_and_tags(
+            tags, ingredients, recipe
+        )
+
+    def update(self, instance, validated_data):
+        instance.ingredients.clear()
+        instance.tags.clear()
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags')
+        instance = super().update(instance, validated_data)
+        return self.add_ingredients_and_tags(
+            tags, ingredients, instance
+        )
