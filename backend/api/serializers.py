343667d51a6703202f958be2fc1cade99493ea0f
Алёна Перова <perovaai31@yandex.ru>
2023-09-12 18:20:26 +0300

    + вьюха и селиализатор для чтения рецепта

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from django.db.models import F
 
-from recipes.models import Ingredient, Tag
+from rest_framework.fields import SerializerMethodField
+from rest_framework.serializers import ModelSerializer
+from drf_extra_fields.fields import Base64ImageField
+
+
+from recipes.models import Ingredient, Recipe, Tag
+
+
+class UserSerializer(ModelSerializer):
+    pass
 
 
 class IngredientSerializer(ModelSerializer):
@@ -15,3 +24,57 @@ class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
+
+class RecipeShortSerializer(ModelSerializer):
+    '''Сериализатор для вывода краткого содержания рецепта.'''
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'image',
+            'cooking_time'
+        )
+
+
+class RecipeReadSerializer(ModelSerializer):
+    '''Сериализатор для просмотра рецептов.'''
+    tags = TagSerializer(many=True, read_only=True)
+    author = UserSerializer(read_only=True)
+    ingredients = SerializerMethodField()
+    image = Base64ImageField()
+    is_favorited = SerializerMethodField(read_only=True)
+    is_in_shopping_cart = SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+    def get_ingredients(self, obj):
+        recipe = obj
+        ingredients = recipe.ingredients.values(
+            'id',
+            'name',
+            'measurement_unit',
+            amount=F('ingredienttorecipe__amount')
+        )
+        return ingredients
+
+    def get_is_favorited(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return user.fav_recipe.filter(recipe=obj).exists()
+
+    def get_is_in_shopping_cart(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return user.shopping_cart.filter(recipe=obj).exists()
+
+
+class RecipeWriteSerializer(ModelSerializer):
+    pass
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from recipes.views import IngredientViewSet, TagViewSet
+from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 router = DefaultRouter()
 
 router.register('tags', TagViewSet, basename='tag'),
 router.register('ingredients', IngredientViewSet, basename='ingredient'),
-# router.register('recipes', )
+router.register('recipes', RecipeViewSet, basename='recipe')
 
 urlpatterns = [
     path('', include(router.urls)),
