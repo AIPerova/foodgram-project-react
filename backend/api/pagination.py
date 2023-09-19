900b5133946f510a1a4535f42d6b89b10e5856fb
Алёна Перова <perovaai31@yandex.ru>
2023-09-19 13:04:38 +0300

    проверка

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class LimitPageNumberPagination(PageNumberPagination):
     '''Переопределение пагинации с лимитом. '''
-    page_size = 6
+    page_size = 10
     page_size_query_param = 'limit'
